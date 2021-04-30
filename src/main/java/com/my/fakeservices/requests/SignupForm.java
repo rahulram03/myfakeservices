db7feceda61195915e1a823d341f29a5bb9f0251
Rahul Kumar Ram <rahulram03@gmail.com>
2021-04-30 19:57:41 +1000

    First commit now one

--- src/main/java/com/my/fakeservices/requests/SignupForm.java
+++ src/main/java/com/my/fakeservices/requests/SignupForm.java
@@ -7,9 +7,6 @@ import javax.validation.constraints.NotBlank;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import lombok.Data;
-
-@Data
 public class SignupForm {
 
     @NotBlank
@@ -30,5 +27,57 @@ public class SignupForm {
 	
 	@Transient
 	private MultipartFile file;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public MultipartFile getFile() {
+		return file;
+	}
+
+	public void setFile(MultipartFile file) {
+		this.file = file;
+	}
+	
+	
+	
+	
 
 }
